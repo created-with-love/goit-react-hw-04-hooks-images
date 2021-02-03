b9826694f74195d22a1b53391b249428cdb84358
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-02-03 13:42:57 +0200

    switch async request type for generic

--- src/services/gallery-service.ts
+++ src/services/gallery-service.ts
@@ -13,17 +13,14 @@ interface IResponeObj {
   [key: string]: any;
 }
 
-type fetchType = (query: string, currentPage: number) => Promise<IResponeObj[]>;
+async function http<T>(query: string, currentPage: number): Promise<T> {
+  const response = await axios.get(
+    `${BASE_URL}?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+  );
+  return await response.data.hits;
+}
 
-const fetchGallery: fetchType = (query, currentPage) => {
-  return axios
-    .get(
-      `${BASE_URL}?q=${query}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-    )
-    .then(response => {
-      console.log(response.data.hits);
-      return response.data.hits;
-    });
-};
+const fetchGallery = (query: string, currentPage: number) =>
+  http<IResponeObj[]>(query, currentPage);
 
 export default fetchGallery;
